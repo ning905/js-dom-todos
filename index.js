f2e22bdca5e4fd17cca1f5442d019941fc6bf414
Ning Ma <ningma905@gmail.com>
2022-07-04 19:31:59 +0100

    complete

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 const todosUL = document.getElementById("todo-list");
 const form = document.querySelector("form");
 
-let state = [];
+let state;
 const updateState = (updated) => {
   state = [...updated];
 };
 
-const getTodos = () => {
-  //  console.log("getTodos() called");
-  fetch("http://localhost:3000/todos")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (todos) {
-      updateState(todos);
-      //console.log("todos updated", state);
-      createTodoList();
-    });
+//Complete button
+const createCompleteBtn = (todo) => {
+  const btn = document.createElement("button");
+  btn.style.marginLeft = "1rem";
+  getCompleteBtnText(btn, todo);
+
+  listenToCompleteBtn(btn, todo);
+
+  return btn;
 };
 
-const postTodo = (todo) => {
-  fetch("http://localhost:3000/todos", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: todo,
-      completed: false,
-    }),
-  });
+const getCompleteBtnText = (btn, todo) => {
+  if (todo.completed === true) {
+    btn.innerText = "Incomplete";
+  } else {
+    btn.innerText = "Complete";
+  }
 };
 
-const completeTodo = (id) => {
-  fetch(`http://localhost:3000/todos/${id}`, {
+const setTodoComplete = (todo) => {
+  fetch(`http://localhost:3000/todos/${todo.id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
@@ -42,8 +35,8 @@ const completeTodo = (id) => {
   });
 };
 
-const incompleteTodo = (id) => {
-  fetch(`http://localhost:3000/todos/${id}`, {
+const setTodoIncomplete = (todo) => {
+  fetch(`http://localhost:3000/todos/${todo.id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
@@ -52,89 +45,93 @@ const incompleteTodo = (id) => {
   });
 };
 
-const toggleTodoStatus = (todo) => {
-  if (todo.completed === true) {
-    incompleteTodo(todo.id);
-  } else {
-    completeTodo(todo.id);
-  }
-};
-
-const toggleBtnText = (btn, todo) => {
-  if (todo.completed === true) {
-    btn.innerText = "Incomplete";
-  } else {
-    btn.innerText = "Complete";
-  }
-};
-
-const deleteTodo = (id) => {
-  fetch(`http://localhost:3000/todos/${id}`, {
-    method: "DELETE",
-  });
-};
-
-const createCompleteBtn = (todo) => {
-  const btn = document.createElement("button");
-  btn.classList.add("complete-btn");
-  btn.style.marginLeft = "1rem";
-  toggleBtnText(btn, todo);
-
+const listenToCompleteBtn = (btn, todo) => {
   btn.addEventListener("click", () => {
-    console.log(todo);
-    toggleBtnText(btn, todo);
-    toggleTodoStatus(todo);
+    if (todo.completed === true) {
+      setTodoIncomplete(todo);
+    } else {
+      setTodoComplete(todo);
+    }
 
-    render();
+    readTodo();
   });
-
-  return btn;
 };
 
+//Delete button
 const createDeleteBtn = (todo) => {
   const btn = document.createElement("button");
   btn.innerText = "Delete";
-  btn.classList.add("delete-btn");
   btn.style.marginLeft = "1rem";
 
-  btn.addEventListener("click", (e) => {
-    deleteTodo(todo.id);
-
-    render();
-  });
+  listenToDeleteBtn(btn, todo);
 
   return btn;
 };
 
+const listenToDeleteBtn = (btn, todo) => {
+  btn.addEventListener("click", () => {
+    fetch(`http://localhost:3000/todos/${todo.id}`, {
+      method: "DELETE",
+    });
+
+    readTodo();
+  });
+};
+
+//HTML
 const createTodo = (todo) => {
   const li = document.createElement("li");
   li.innerText = todo.title;
+
   if (todo.completed) {
     li.classList.add("completed");
   }
-  li.append(createCompleteBtn(todo));
-  li.append(createDeleteBtn(todo));
-  return li;
+
+  li.append(createCompleteBtn(todo), createDeleteBtn(todo));
+
+  todosUL.append(li);
 };
 
 const createTodoList = () => {
   todosUL.innerHTML = "";
-  for (const todo of state) {
-    todosUL.append(createTodo(todo));
-  }
+  state.forEach((todo) => createTodo(todo));
 };
 
-const render = () => {
-  getTodos();
+const listenToForm = () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const todo = {
+      title: form.title.value,
+      completed: false,
+    };
+
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    })
+      .then((res) => res.json())
+      .then((todo) => createTodo(todo));
+  });
+
+  form.reset();
 };
 
-render();
+const readTodo = () => {
+  fetch("http://localhost:3000/todos")
+    .then((res) => res.json())
+    .then((todos) => {
+      updateState(todos);
+      createTodoList();
+    });
+};
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const todo = e.target[0].value;
-  postTodo(todo);
-  setTimeout(() => {
-    render();
-  }, 0);
-});
+const init = () => {
+  listenToForm();
+
+  readTodo();
+};
+
+init();
